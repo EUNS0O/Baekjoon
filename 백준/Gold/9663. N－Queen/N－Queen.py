4fac8dec0aa6b9b6ff36e775c52d73bd4be5e2b4
EUNS0O <jangeunsu9505@gmail.com>
2025-03-20 14:18:03 +0900

    [Gold IV] Title: N-Queen, Time: 20032 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,28 +1,25 @@
-import math
-import sys
-import queue
-import copy
-from itertools import combinations
-
 n = int(input())
 
-lst1 = [False for _ in range(20)]
-lst2 = [False for _ in range(50)]
-lst3 = [False for _ in range(50)]
-ans = 0
+li1 = [False for _ in range(20)] #column
+li2 = [False for _ in range(50)] #오른쪽 대각선
+li3 = [False for _ in range(50)] #왼쪽 대각선
+ans =  0
+
 def func(r):
     global ans
-    if r == n:
-        ans+=1
+    if r==n:
+        ans += 1
+           
+    
     for i in range(n):
-        if not lst1[i] and not lst2[r + i] and not lst3[r - i + n - 1]:
-            lst1[i] = True
-            lst2[r + i] = True
-            lst3[r - i + n - 1] = True
+        if not li1[i] and not li2[r+i] and not li3[r-i+n-1]:
+            li1[i] = True
+            li2[r+i] = True
+            li3[r-i+n-1] = True
             func(r+1)
-            lst1[i] = False
-            lst2[r + i] = False
-            lst3[r - i + n - 1] = False
-
+            li1[i] = False
+            li2[r+i] = False
+            li3[r-i+n-1] = False
+            
 func(0)
-print(ans)
+print(ans) 
