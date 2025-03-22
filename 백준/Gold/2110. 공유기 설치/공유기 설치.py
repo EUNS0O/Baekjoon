ea4fcd4228ca18eef6cd264ca38b8fd2bcfcf102
EUNS0O <jangeunsu9505@gmail.com>
2025-03-23 00:39:01 +0900

    [Gold IV] Title: 공유기 설치, Time: 312 ms, Memory: 42728 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,30 +1,26 @@
-n,c = map(int,input().split())
-houses=[]
+import sys
 
-for i in range(n):
-    houses.append(int(input()))
- 
-houses.sort()
-start = 1
-end = houses[-1] - houses[0]
-answer = 0
+def gongugi(left, right, target):
+    best = 0
+    while left <= right: 
+        mid = (left + right) // 2
+        count = 1
+        last_install = h[0]
+        for home in h : 
+            if home -last_install >= mid:
+                count += 1
+                last_install = home 
+        if count >= target:
+            best = mid
+            left = mid + 1
+        else: 
+            right = mid - 1
+    return best
 
-while start <= end: 
-    mid = (start+end)//2
-    current = houses[0]
-    count = 1
-
-    for i in range(1, len(houses)):
-        if houses[i] >=  current + mid: #지금 집+설치 간격의 거리보다 같거나 머냐
-            count += 1 #설치
-            current = houses[i] #지금 위치 업데이트
-            
-    if count>=c: #설치한 공유기가 목표개수 c개를 넘어 더 넓게 설치할 수 있다는 얘기. 
-    
-        start = mid + 1 #설치거리 늘리기
-        answer = mid #중간 거리
-    else: #설치 개수가 총 공유기 개수보다 작을 때(간격을 너무 크게 잡았을 때)
-        end = mid - 1 #설치거리 줄이기
-
-
-print(answer) 
+N, C = map(int, sys.stdin.readline().split())
+hh = [int(sys.stdin.readline()) for _ in range(N)]
+h = sorted(hh)
+left = 0
+right = max(h)
+result = gongugi(left,right,C)
+print(result)
