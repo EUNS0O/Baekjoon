56f6438d7a2e13696adc6b23e68526a66038a3b8
EUNS0O <jangeunsu9505@gmail.com>
2025-03-24 11:45:17 +0900

    [Gold IV] Title: 사냥꾼, Time: 3492 ms, Memory: 43332 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,17 +1,25 @@
 m,n,l = map(int,input().split()) 
 #m: 사대의 수 n:동물의 수 l:사정거리
 m_list = list(map(int,input().split())) #사대 위치
+m_list.sort()
 
 count = 0
-
-for i in range(n):
-    x, y = map(int, input().split())
+for _ in range(n):
+    x, y = map(int,input().split())
+    left = 0
+    right = m-1
     
-    for s in m_list:
-        dis = abs(s - x) + y
+    while left<=right:
+        
+        mid = (left + right)//2
+        dis = abs(m_list[mid]-x)+y
         
         if dis <= l:
-            count +=1
-            break
-        
+            count += 1
+            break    
+        elif x < m_list[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
 print(count)
